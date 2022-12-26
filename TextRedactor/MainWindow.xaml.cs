e6add4ceb93c162031030bea2615cc4d799a4655
Кирилл Зайцев <moja.orbita@ya.ru>
2022-12-26 13:18:11 +0300

    Add Themes

--- TextRedactor/MainWindow.xaml.cs
+++ TextRedactor/MainWindow.xaml.cs
@@ -145,5 +145,13 @@ namespace WpfApp1_lab3
                 File.WriteAllText(saveFileDialog.FileName, textBox.Text);
             }
         }
+
+        private void themes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Uri theme = new Uri(themes.SelectedIndex == 0 ? "Light.xaml" : "Dark.xaml", UriKind.Relative);
+            ResourceDictionary themeDict = Application.LoadComponent(theme) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(themeDict);
+        }
     }
 }
